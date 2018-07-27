ee2eafa62b6c331280e0ee6e85c2501aa639e6ca
EdvardGB <edvard_bakken@live.no>
2018-07-27 14:14:29 +0200

    checking push permissions

--- src/main/resources/services/imageService/imageService.js
+++ src/main/resources/services/imageService/imageService.js
@@ -145,7 +145,6 @@ exports.put = function(req) {
         type: portalLib.getMultipartText('type')
     }
     
-    
     var source = portalLib.getMultipartText('source')
 
     var file = portalLib.getMultipartItem('file')
